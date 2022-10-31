9c05de5debcdbd285a6dd21acd3a838333afa7ac
Your Name <pietroannobil@Pietros-MacBook-Air.local>
2022-10-31 18:27:49 +0000

    up

---
+++ src/main/java/Test/BookTest.java
@@ -0,0 +1,33 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import model.Book;
+
+class BookTest {
+
+	@BeforeEach
+	void alertMessage() {
+		System.out.println("Test completed");
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	//String bookTitle, String bookAuthor, Integer bookYear, Double bookCost
+	@Test
+	@DisplayName("check book author")
+	void checkUser() {
+		Book b1 = new Book("The new Book","Steven Lee",2004,33.50);
+		String expectedResult = "Steven Lee";
+		String actual = b1.getBookAuthor();
+		Assert.assertEquals(expectedResult,actual);
+	}
+}
